3888a2bef9846add9631c4aa895022c4c9aa0632
JVW-Hue <janovanwyk553@gmail.com>
2025-12-04 23:30:34 +0200

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/AI-YouTube-Title-Optimizer-',
   experimental: {
     appDir: true,
   },
   images: {
+    unoptimized: true,
     domains: ['lh3.googleusercontent.com'],
   },
 }
